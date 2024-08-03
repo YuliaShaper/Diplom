02d81a24a658990db30cf2d9fb3db45e620cac49
Yulia <yulia.anpilowa@yandex.ru>
2024-08-03 17:16:27 +1000

    Diplom

---
+++ diplom_movie/movies/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from django.views.generic.base import View
+
+from .models import Movie
+
+
+class MoviesView(View):
+    def get(self, request):
+        movies = Movie.objects.all()
+        return render(request, "movies/movies.html", {"movie_list": movies})
+
+
+class MovieDetailView(View):
+    def get(self, request, slug):
+        movie = Movie.objects.get(url=slug)
+        return render(request, "movies/movie_detail.html", {"movie": movie})
+
+
+class AddReview(View):
+    def post(self, request, pk):
+        print(request.POST)
+        return redirect("/")
